5b8315f09064a260f325317a8d1d99ba4f96020a
4Miguel2 <delgdevoleper@gmail.com>
2024-02-01 18:17:05 -0300

    Colecoes pt 01 - equals pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -0,0 +1,42 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.dominio;
+
+import java.util.Objects;
+
+public class Smartphone {
+    private String serialNumber;
+    private String marca;
+
+    public Smartphone(String serialNumber, String marca) {
+        this.serialNumber = serialNumber;
+        this.marca = marca;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Smartphone that = (Smartphone) o;
+        return Objects.equals(serialNumber, that.serialNumber) && Objects.equals(marca, that.marca);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(serialNumber, marca);
+    }
+}
